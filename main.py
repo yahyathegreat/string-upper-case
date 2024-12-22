56a4194b6404794a20d9912b361c5c7f3156e17b
yahyathegreat <yahyamardan1234@gmail.com>
2024-12-22 15:00:17 +0500

    ahhh

--- main.py
+++ main.py
@@ -8,4 +8,16 @@ class IOString():
 str1 = IOString()
 str1.get_String()
 str1.print_String()
-
+class Employee:
+     def __init__(self):
+          print('employee created')
+     def __del__(self):
+          print("Destructor called")
+def create_obj():
+     print('making object...')
+     obj = Employee()
+     print('function end...')
+     return obj
+print('Calling Create_obj() function...')
+obj = create_obj()
+print('Program end')
